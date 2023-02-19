ff7f4d4578a0d6ef9d86687a9a3e86c863e5366f
rstar <rstar0823@icloud.com>
2023-02-19 16:16:48 +0900

    add: UserServiceTestを追加

---
+++ src/tests/Unit/Service/UserServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Service;
+
+use tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
