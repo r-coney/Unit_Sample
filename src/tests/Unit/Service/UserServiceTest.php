986e1df39836f36d1aaa476ceaecea3d6ac358f1
rstar <rstar0823@icloud.com>
2023-02-19 17:13:39 +0900

    feat: ユーザー検索のUnitテストを追加

--- src/tests/Unit/Service/UserServiceTest.php
+++ src/tests/Unit/Service/UserServiceTest.php
@@ -2,17 +2,29 @@
 
 namespace Tests\Unit\Service;
 
-use tests\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserServiceTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function IDに対応するユーザーを取得できること()
     {
-        $this->assertTrue(true);
+        // ユーザーを作成する
+        $expectedUser = User::factory()->create();
+
+        // 作成したユーザーのidで検索する
+        $userModel = new User();
+        $service = new UserService($userModel);
+        $targetUser = $service->findById($expectedUser->id);
+
+        // 作成したユーザーと取得したユーザーを検証
+        $this->assertSame($expectedUser->id, $targetUser->id);
     }
 }
