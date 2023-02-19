c906208b1be196acbba77093266c830fcabfa840
rstar <rstar0823@icloud.com>
2023-02-19 17:16:08 +0900

    feat: ユーザー検索のメソッドを追加

--- src/app/Services/UserService.php
+++ src/app/Services/UserService.php
@@ -3,7 +3,37 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\User;
+use InvalidArgumentException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 class UserService
 {
+    private $user;
 
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * IDに対応するUserモデルを取得
+     *
+     * @param integer $id
+     * @return User
+     */
+    public function findById(int $id): User
+    {
+        if (is_null($id)) {
+            throw new InvalidArgumentException('User ID dose not exist');
+        }
+
+        $result = $this->user->find($id);
+
+        if (is_null($result)) {
+            throw new NotFoundHttpException("User with ID {$id} does not exist");
+        }
+
+        return $result;
+    }
 }
